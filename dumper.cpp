1f36b23abe9084f7b150527465c20e6d06aee7b1
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-04-02 14:15:17 -0400

    Update dumper.cpp

--- dumper.cpp
+++ dumper.cpp
@@ -1,30 +1,30 @@
+//dont use luac if u have 1 braincell you should at least know some things dont exist
+//assuming your using lua not LUAU
 extern "C" {
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 }
 
+const char* scriptsource = "print('i use axon')";
+
 int main() {
-    // Create a new Lua state
     lua_State* L = luaL_newstate();
 
-    // Load the Lua script
-    luaL_loadstring(L, "print('Hello, world!')");
+    luaL_loadstring(L, scriptsource);//luau doesnt have this..use luau load or something
 
-    // Dump the Lua script to bytecode
-    size_t len;
-    const char* bytecode = lua_dump(L, [](lua_State*, const void* p, size_t sz, void* ud) {
+    size_t len;//size
+    const char* bytecode = lua_dump(L, [](lua_State*, const void* p, size_t sz, void* ud) {//get bytecode
         *(std::string*)ud += std::string((const char*)p, sz);
         return 0;
     }, new std::string(""), &len);
 
-    // Print the bytecode
-    for (size_t i = 0; i < len; i++) {
+    printf("your shitty bytecode ->\n\n");
+    for (size_t i = 0; i < len; i++) {//return bytecode
         printf("%02X ", bytecode[i] & 0xFF);
     }
     printf("\n");
 
-    // Clean up
     delete (std::string*)lua_touserdata(L, lua_upvalueindex(1));
     lua_close(L);
 
